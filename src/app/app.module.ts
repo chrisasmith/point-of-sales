b277e56a44252b30322a4ae489aff0748764f916
Chris Smith <chris.smith@uniguest.com>
2019-08-19 07:43:29 -0500

    Init commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {PointOfSalesModule} from './point-of-sales/point-of-sales.module';
+import {TimeService} from './services/time.service';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -10,9 +14,13 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+
+    PointOfSalesModule
   ],
-  providers: [],
+  providers: [TimeService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
